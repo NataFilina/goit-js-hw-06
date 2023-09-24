4228dca25915b97241a5564792b2c364e3a449ec
NataFilina <filinanatash@gmail.com>
2023-09-24 23:58:27 +0300

    task 9

--- js/task-09.js
+++ js/task-09.js
@@ -1,5 +1,16 @@
+const btn = document.querySelector('.change-color');
+const color = document.querySelector('.color');
+const body = document.querySelector('body');
+
+btn.addEventListener('click', onChangeSize);
+
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215)
     .toString(16)
     .padStart(6, 0)}`;
+}
+
+function onChangeSize() {
+  body.style.backgroundColor = getRandomHexColor();
+  color.textContent = getRandomHexColor();
 }
--- js/task-10.js
+++ js/task-10.js
@@ -1,5 +1,20 @@
+const input = document.querySelector('#controls > input');
+const btnCreate = document.querySelector('[data-create]');
+const btnDestroy = document.querySelector('[data-destroy]');
+
+btnCreate.addEventListener('click', onCreateElements);
+btnDestroy.addEventListener('click', onDestroyElements);
+
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215)
     .toString(16)
     .padStart(6, 0)}`;
+}
+
+function createBoxes(amount) {
+  const div = document.createElement('div');
+  amount = Number(input.currentTarget.value);
+  // body.style.backgroundColor = getRandomHexColor();
+  // color.textContent = getRandomHexColor();
+  console.log(amount);
 }
